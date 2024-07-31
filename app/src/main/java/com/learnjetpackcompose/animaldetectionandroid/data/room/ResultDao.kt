b4eed42b9d1c3e4d988b7e65929304473b498430
Airi <airiagustus82@gmail.com>
2024-08-01 02:05:54 +0700

    Add Result Dao

--- app/src/main/java/com/learnjetpackcompose/animaldetectionandroid/data/room/ResultDao.kt
+++ app/src/main/java/com/learnjetpackcompose/animaldetectionandroid/data/room/ResultDao.kt
@@ -1,2 +1,38 @@
 package com.learnjetpackcompose.animaldetectionandroid.data.room
 
+import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.TypeConverter
+import com.learnjetpackcompose.animaldetectionandroid.data.entity.ResultEntity
+
+@Dao
+interface ResultDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun saveResult(resultEntity: ResultEntity): Long
+
+    @Query("SELECT * FROM ResultEntity WHERE image_uri = :imageUri LIMIT 1")
+    fun getResultByUri(imageUri: String): ResultEntity?
+
+    @Query("SELECT * FROM ResultEntity")
+    fun showResult(): LiveData<List<ResultEntity>>
+
+    @Delete
+    fun deleteResult(resultEntity: ResultEntity)
+}
+
+class UriConverter {
+    @TypeConverter
+    fun fromUri(uri: Uri?): String? {
+        return uri?.toString()
+    }
+
+    @TypeConverter
+    fun toUri(uriString: String?): Uri? {
+        return uriString?.let { Uri.parse(it) }
+    }
+}
