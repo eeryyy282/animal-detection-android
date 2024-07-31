3005a0e8d8226522b7e663ff2a19f4b46233a44e
Airi <airiagustus82@gmail.com>
2024-08-01 02:13:54 +0700

    Add Result Repository and App Executors

--- app/src/main/java/com/learnjetpackcompose/animaldetectionandroid/data/repository/ResultRepository.kt
+++ app/src/main/java/com/learnjetpackcompose/animaldetectionandroid/data/repository/ResultRepository.kt
@@ -1,4 +1,51 @@
 package com.learnjetpackcompose.animaldetectionandroid.data.repository
 
-class ResultRepository {
+import androidx.lifecycle.LiveData
+import com.learnjetpackcompose.animaldetectionandroid.data.entity.ResultEntity
+import com.learnjetpackcompose.animaldetectionandroid.data.room.ResultDao
+import com.learnjetpackcompose.animaldetectionandroid.utils.AppExecutors
+
+class ResultRepository(
+    private val resultDao: ResultDao,
+    private val appExecutors: AppExecutors
+) {
+    fun saveResult(resultEntity: ResultEntity) {
+        appExecutors.diskIO.execute {
+            resultDao.saveResult(resultEntity)
+        }
+    }
+
+    fun removeResult(resultEntity: ResultEntity) {
+        appExecutors.diskIO.execute {
+            resultDao.deleteResult(resultEntity)
+        }
+    }
+
+    fun showResult(): LiveData<List<ResultEntity>> {
+        return resultDao.showResult()
+    }
+
+    fun getResultByUri(imageUri: String, callback: (ResultEntity?) -> Unit) {
+        appExecutors.diskIO.execute {
+            val resultEntity = resultDao.getResultByUri(imageUri)
+            appExecutors.mainThread.execute {
+                callback(resultEntity)
+            }
+        }
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ResultRepository? = null
+
+        fun getInstance(
+            resultDao: ResultDao,
+            appExecutors: AppExecutors
+        ): ResultRepository =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: ResultRepository(resultDao, appExecutors)
+            }.also { INSTANCE = it }
+
+    }
+
 }
