a07ab62bc786c432e1e41db2f6c28ddddc451b6c
Airi <airiagustus82@gmail.com>
2024-08-01 02:08:02 +0700

    Add Result Database

--- app/src/main/java/com/learnjetpackcompose/animaldetectionandroid/data/room/ResultDatabase.kt
+++ app/src/main/java/com/learnjetpackcompose/animaldetectionandroid/data/room/ResultDatabase.kt
@@ -1,4 +1,33 @@
 package com.learnjetpackcompose.animaldetectionandroid.data.room
 
-class ResultDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.learnjetpackcompose.animaldetectionandroid.data.entity.ResultEntity
+
+@Database(entities = [ResultEntity::class], version = 1)
+@TypeConverters(UriConverter::class)
+abstract class ResultDatabase : RoomDatabase() {
+    abstract fun resultDao(): ResultDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ResultDatabase? = null
+
+        @JvmStatic
+        fun getInstace(context: Context): ResultDatabase {
+            if (INSTANCE == null) {
+                synchronized(ResultDatabase::class.java) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        ResultDatabase::class.java, "result_database"
+                    )
+                        .build()
+                }
+            }
+            return INSTANCE as ResultDatabase
+        }
+    }
 }
