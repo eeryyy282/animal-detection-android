78ec0bbd256a87583b08ab47bb1ef0394e0426d2
Airi <airiagustus82@gmail.com>
2024-08-01 02:01:44 +0700

    Add data class for result entity

--- app/src/main/java/com/learnjetpackcompose/animaldetectionandroid/data/entity/ResultEntity.kt
+++ app/src/main/java/com/learnjetpackcompose/animaldetectionandroid/data/entity/ResultEntity.kt
@@ -1,4 +1,26 @@
 package com.learnjetpackcompose.animaldetectionandroid.data.entity
 
-class ResultEntity {
-}
+import android.net.Uri
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class ResultEntity(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Long = 0,
+
+    @ColumnInfo(name = "image_uri")
+    var imageUri: Uri,
+
+    @ColumnInfo(name = "timestamps")
+    var timeStamps: String,
+
+    @ColumnInfo(name = "predicted_class")
+    var predictedClass: String,
+
+    @ColumnInfo(name = "confidence_score")
+    var confidenceScore: String
+
+)
