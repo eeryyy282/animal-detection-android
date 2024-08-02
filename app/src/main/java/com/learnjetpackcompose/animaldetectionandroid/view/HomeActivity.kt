6ad22e4b7816afa5b738c9499fa58a612b31c59b
Airi <airiagustus82@gmail.com>
2024-08-03 01:23:06 +0700

    Add function for HomeActivity

--- app/src/main/java/com/learnjetpackcompose/animaldetectionandroid/view/HomeActivity.kt
+++ app/src/main/java/com/learnjetpackcompose/animaldetectionandroid/view/HomeActivity.kt
@@ -1,21 +1,38 @@
 package com.learnjetpackcompose.animaldetectionandroid.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.learnjetpackcompose.animaldetectionandroid.R
+import com.learnjetpackcompose.animaldetectionandroid.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        setupAction()
+    }
+
+    private fun setupAction() {
+        binding.buttonStartDetection.setOnClickListener {
+            intent = Intent(this@HomeActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
